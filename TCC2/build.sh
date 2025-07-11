2c0dcb4274888d0c77631b95740a3d93ac1bffeb
marcs-feh <marcosfehlauer@protonmail.com>
2025-07-11 17:13:56 -0300

    Add build script, better naming

---
+++ TCC2/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+set -eu
+latexmk -interaction=nonstopmode main.tex
+
+
--- TCC2/clean.sh
+++ TCC2/clean.sh
@@ -4,12 +4,16 @@ set -eu
 TrashFiles(){
 	find .  -name '*.aux' \
 		-or -name '*.log' \
+		-or -name '*.fls' \
 		-or -name '*.loq' \
 		-or -name '*.loe' \
 		-or -name '*.bbl' \
 		-or -name '*.blg' \
 		-or -name '*.lof' \
-		-or -name '*.toc'
+		-or -name '*.toc' \
+		-or -name '*.dvi' \
+		-or -name '*.ps' \
+		-or -name '*.fdb_*'
 }
 
 files="$(TrashFiles)"
